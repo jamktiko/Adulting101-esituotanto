565d3aa356491614b4f1ca916a7c3a52b0765c8b
Milena Madlin <ag2404@student.jamk.fi>
2026-04-23 10:56:14 +0300

    varmistettu .gitignore-toimivuus ja server.js polku korjattu

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,8 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
+console.log("TARKISTUS: MONGODB_URI on:", process.env.MONGODB_URI ? "Löytyi!" : "TYHJÄ (undefined)");
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors'); // Hyvä lisätä tässä vaiheessa Angularia varten
